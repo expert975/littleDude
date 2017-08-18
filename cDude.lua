a9121173e6b6bd0144c5671cc094339a16ef6730
expert975 <rodrigocomputadores125@hotmail.com>
2017-08-18 15:45:57 -0300

    Convert all double spaces to tabs

--- cDude.lua
+++ cDude.lua
@@ -1,22 +1,22 @@
 --[[
 	LittleDude:
-    This script shows how to draw a relative distance indicator in MTA
+		This script shows how to draw a relative distance indicator in MTA
 
 
-  Copyright 2017 Rodrigo Martins
+	Copyright 2017 Rodrigo Martins
 
 	This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+		it under the terms of the GNU General Public License as published by
+		the Free Software Foundation, either version 3 of the License, or
+		(at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+		This program is distributed in the hope that it will be useful,
+		but WITHOUT ANY WARRANTY; without even the implied warranty of
+		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+		GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+		You should have received a copy of the GNU General Public License
+		along with this program. If not, see <http://www.gnu.org/licenses/>.
 ]]
 
 local safeZoneRadius = 10 --radius of our safe zone
@@ -33,29 +33,29 @@ local littleDude = guiCreateStaticImage (.7, .89, .07, .07, "img/running.png", t
 
 
 local function mapValues(x, in_min, in_max, out_min, out_max) --rescales values
-  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 end
 
 local getDistanceBetweenPoints2D = getDistanceBetweenPoints2D
 local localPlayer = localPlayer
 local math = math
 local function calculateLittleDudeDistance()
-  local px, py = localPlayer.position.x, localPlayer.position.y --player coordinates
-  local sx, sy = safeAreaCol.position.x, safeAreaCol.position.y --safe area coords
-  distanceToSafeArea = (getDistanceBetweenPoints2D(px, py, sx, sy) - safeZoneRadius > 0) and
-  getDistanceBetweenPoints2D(px, py, sx, sy) - safeZoneRadius or 0 --show positive or 0
-  if distanceToSafeArea > maxDistance then --if too far
-    return .7 --stay at max
-  else
-    return mapValues(distanceToSafeArea, 0, maxDistance, .9, .7) --calculate relative distance
-  end
+	local px, py = localPlayer.position.x, localPlayer.position.y --player coordinates
+	local sx, sy = safeAreaCol.position.x, safeAreaCol.position.y --safe area coords
+	distanceToSafeArea = (getDistanceBetweenPoints2D(px, py, sx, sy) - safeZoneRadius > 0) and
+	getDistanceBetweenPoints2D(px, py, sx, sy) - safeZoneRadius or 0 --show positive or 0
+	if distanceToSafeArea > maxDistance then --if too far
+		return .7 --stay at max
+	else
+		return mapValues(distanceToSafeArea, 0, maxDistance, .9, .7) --calculate relative distance
+	end
 end
 
 local guiSetPosition = guiSetPosition
 local dxDrawText = dxDrawText
 local function moveLittleDude() --moves littleDude
-  littleDudeDistance = calculateLittleDudeDistance()
-  guiSetPosition(littleDude, littleDudeDistance, .89, true) --set littleDudes position
-  dxDrawText(math.floor(distanceToSafeArea), screenx*(littleDudeDistance + .027) , screeny*.96) --draw distance
+	littleDudeDistance = calculateLittleDudeDistance()
+	guiSetPosition(littleDude, littleDudeDistance, .89, true) --set littleDudes position
+	dxDrawText(math.floor(distanceToSafeArea), screenx*(littleDudeDistance + .027) , screeny*.96) --draw distance
 end
 addEventHandler("onClientRender", root, moveLittleDude)
